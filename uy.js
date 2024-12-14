3f09731476e636611139d4e1bb4b26897335b2ce
Umidjon Umedov <lustrum062@gmail.com>
2024-12-14 13:13:36 +0300

    homework

---
+++ uy.js
@@ -0,0 +1,65 @@
+//1misol
+// function sortDrinkByPrice(drinks) {
+//   return drinks.sort((a, b) => a.price - b.price);
+// }
+// const drinks = [
+//   { name: "lemonade", price: 50 },
+//   { name: "lime", price: 10 },
+//   { name: "juice", price: 13 },
+//   { name: "alcohol", price: 60 },
+// ];
+
+// console.log(sortDrinkByPrice(drinks));
+
+//2misol
+// function objectToArray(obj) {
+//   return Object.entries(obj);
+// }
+// console.log(
+//   objectToArray({
+//     D: 1,
+//     B: 2,
+//     C: 3,
+//   })
+// );
+// console.log(
+//   objectToArray({
+//     likes: 2,
+//     dislikes: 3,
+//     followers: 10,
+//   })
+// );
+
+//3misol
+// let arr=[]
+// let users = [
+//     { name: "John", email: "john@example.com" },
+//     { name: "Jason", email: "jason@example.com" },
+//     { name: "Jeremy", email: "jeremy@example.com" },
+//     { name: "Jacob", email: "jacob@example.com" }
+//   ]
+// for(let i=0;i<users.length;i++){
+//     arr.push(users[i].name)
+// }
+// console.log(arr);
+
+//4misol
+// function afterNYears(person, n) {
+//   for (let element in person) {
+//     person[element] += n;
+//   }
+//   return person;
+// }
+
+// console.log(
+//   afterNYears(
+//     {
+//       Joel: 32,
+//       Fred: 44,
+//       Reginald: 65,
+//       Susan: 33,
+//       Julian: 13,
+//     },
+//     8
+//   )
+// );
